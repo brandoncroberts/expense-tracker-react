41e08506e27e189b4ee63ed6ce880e9b3a2b792a
brandon roberts <8brandon@protonmail.com>
2020-03-02 20:47:40 +0100

    Add context hook to create a global state

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import { IncomeExpenses } from "./components/IncomeExpenses";
 import { TransactionList } from "./components/TransactionList";
-import { AddTransation } from "./components/AddTransation";
+import { AddTransaction } from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
 function App() {
   return (
-    <div className="">
+    <GlobalProvider>
       <Header />
 
       <div className="container">
         <Balance />
         <IncomeExpenses />
         <TransactionList />
-        <AddTransation />
+        <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
