57e54051226824c1b9b993ce4590b74c4fa12c0d
brandon roberts <8brandon@protonmail.com>
2020-03-02 14:21:45 +0100

    Clean up CRA Files and add components

---
+++ src/components/AddTransation.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+export const AddTransation = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState("");
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form id="form">
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            value={text}
+            onChange={e => setText(e.target.value)}
+            id="text"
+            placeholder="Enter text..."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type="number"
+            id="amount"
+            placeholder="Enter amount..."
+            onChange={e => setAmount(e.target.value)}
+            value={amount}
+          />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  );
+};
